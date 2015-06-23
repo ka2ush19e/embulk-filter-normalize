e1d68157ad001b98996ced3d8714de443664e876
Kazushige Morooka <morokazu@gmail.com>
2015-06-23 13:23:06 +0900

    Modify

--- src/main/java/org/embulk/filter/NormalizeFilterPlugin.java
+++ src/main/java/org/embulk/filter/NormalizeFilterPlugin.java
@@ -61,130 +61,114 @@ public class NormalizeFilterPlugin
     public PageOutput open(TaskSource taskSource, final Schema inputSchema,
             final Schema outputSchema, final PageOutput output)
     {
-        PluginTask task = taskSource.loadTask(PluginTask.class);
-        return new NormalizePageOutput(task, inputSchema, output);
-    }
-
-    private class NormalizePageOutput
-            implements PageOutput
-    {
-        private final Schema schema;
-        private final PageOutput output;
-
-        private final PageReader reader;
-        private final Set<String> normalizeColumns;
-        private final Normalizer.Form form;
-        private final boolean trim;
-
-        public NormalizePageOutput(PluginTask task, Schema schema, PageOutput output)
+        final PluginTask task = taskSource.loadTask(PluginTask.class);
+        return new PageOutput()
         {
-            this.schema = schema;
-            this.output = output;
-
-            reader = new PageReader(schema);
-            normalizeColumns = task.getColumns();
-            form = task.getForm();
-            trim = task.getTrim();
-        }
-
-        @Override
-        public void add(Page page)
-        {
-            reader.setPage(page);
-            try (final PageBuilder builder = new PageBuilder(Exec.getBufferAllocator(), schema, output)) {
-                ColumnVisitor visitor = new NormalizeColumnVisitor(builder);
-                while (reader.nextRecord()) {
-                    schema.visitColumns(visitor);
-                    builder.addRecord();
-                }
-                builder.flush(); // XXX: finish => NullPointerException on next page
-            }
-        }
-
-        @Override
-        public void finish()
-        {
-            output.finish();
-        }
-
-        @Override
-        public void close()
-        {
-            reader.close();
-            output.close();
-        }
-
-        private class NormalizeColumnVisitor
-                implements ColumnVisitor
-        {
-            private final PageBuilder builder;
-
-            public NormalizeColumnVisitor(PageBuilder builder)
-            {
-                this.builder = builder;
-            }
+            private final PageReader reader = new PageReader(inputSchema);
+            private final Set<String> normalizeColumns = task.getColumns();
+            private final Normalizer.Form form = task.getForm();
+            private final boolean trim = task.getTrim();
 
             @Override
-            public void booleanColumn(Column column)
+            public void add(Page page)
             {
-                if (reader.isNull(column)) {
-                    builder.setNull(column);
-                } else {
-                    builder.setBoolean(column, reader.getBoolean(column));
+                reader.setPage(page);
+                try (final PageBuilder builder = new PageBuilder(
+                        Exec.getBufferAllocator(), outputSchema, output)) {
+                    ColumnVisitor visitor = new NormalizeColumnVisitor(builder);
+                    while (reader.nextRecord()) {
+                        inputSchema.visitColumns(visitor);
+                        builder.addRecord();
+                    }
+                    builder.flush(); // XXX: finish => NullPointerException on next page
                 }
             }
 
             @Override
-            public void longColumn(Column column)
+            public void finish()
             {
-                if (reader.isNull(column)) {
-                    builder.setNull(column);
-                } else {
-                    builder.setLong(column, reader.getLong(column));
-                }
+                output.finish();
             }
 
             @Override
-            public void doubleColumn(Column column)
+            public void close()
             {
-                if (reader.isNull(column)) {
-                    builder.setNull(column);
-                } else {
-                    builder.setDouble(column, reader.getDouble(column));
-                }
+                reader.close();
+                output.close();
             }
 
-            @Override
-            public void stringColumn(Column column)
+            class NormalizeColumnVisitor
+                    implements ColumnVisitor
             {
-                if (reader.isNull(column)) {
-                    builder.setNull(column);
-                } else {
-                    if (normalizeColumns.contains(column.getName())) {
-                        String normalized = Normalizer.normalize(reader.getString(column), form);
-                        if (trim) {
-                            normalized = normalized.trim();
-                        }
-                        if (normalized.isEmpty()) {
-                            builder.setNull(column);
-                        } else {
-                            builder.setString(column, normalized);
-                        }
+                private final PageBuilder builder;
+
+                public NormalizeColumnVisitor(PageBuilder builder)
+                {
+                    this.builder = builder;
+                }
+
+                @Override
+                public void booleanColumn(Column column)
+                {
+                    if (reader.isNull(column)) {
+                        builder.setNull(column);
                     } else {
-                        builder.setString(column, reader.getString(column));
+                        builder.setBoolean(column, reader.getBoolean(column));
+                    }
+                }
+
+                @Override
+                public void longColumn(Column column)
+                {
+                    if (reader.isNull(column)) {
+                        builder.setNull(column);
+                    } else {
+                        builder.setLong(column, reader.getLong(column));
+                    }
+                }
+
+                @Override
+                public void doubleColumn(Column column)
+                {
+                    if (reader.isNull(column)) {
+                        builder.setNull(column);
+                    } else {
+                        builder.setDouble(column, reader.getDouble(column));
+                    }
+                }
+
+                @Override
+                public void stringColumn(Column column)
+                {
+                    if (reader.isNull(column)) {
+                        builder.setNull(column);
+                    } else {
+                        if (normalizeColumns.contains(column.getName())) {
+                            String normalized = Normalizer.normalize(reader.getString(column), form);
+                            if (trim) {
+                                normalized = normalized.trim();
+                            }
+                            if (normalized.isEmpty()) {
+                                builder.setNull(column);
+                            } else {
+                                builder.setString(column, normalized);
+                            }
+                        } else {
+                            builder.setString(column, reader.getString(column));
+                        }
+                    }
+                }
+
+                @Override
+                public void timestampColumn(Column column)
+                {
+                    if (reader.isNull(column)) {
+                        builder.setNull(column);
+                    } else {
+                        builder.setTimestamp(column, reader.getTimestamp(column));
                     }
                 }
             }
-
-            @Override
-            public void timestampColumn(Column column)
-            {
-                if (reader.isNull(column)) {
-                    builder.setNull(column);
-                } else {
-                    builder.setTimestamp(column, reader.getTimestamp(column));
-                }
-            }
-        }
+        };
     }
 }
